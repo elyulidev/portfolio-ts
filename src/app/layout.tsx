319a4605acbd9e684e88659b6c9885a04f62ebfc
elyulidev <elyuli.dev@gmail.com>
2024-01-26 15:24:10 +0200

    Fix aos animate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import AOSinit from "@/components/AOS";
 
 const poppins = Poppins({
 	subsets: ["latin"],
@@ -19,6 +20,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en' className='scroll-smooth'>
+			<AOSinit />
 			<body className={poppins.className}>{children}</body>
 		</html>
 	);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Hero from "@/components/Hero";
 import Nav from "../components/Nav";
 import MobileNav from "@/components/MobileNav";
@@ -9,9 +7,6 @@ import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import Testimonial from "@/components/Testimonial";
 import Footer from "@/components/Footer";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { useEffect } from "react";
 
 interface Props {
 	searchParams?: { [key: string]: string | string[] | undefined };
@@ -19,33 +14,6 @@ interface Props {
 
 export default function Home({ searchParams }: Props) {
 	const nav = searchParams?.nav === "true" ? true : false;
-
-	useEffect(() => {
-		AOS.init();
-
-		// You can also pass an optional settings object
-		// below listed default settings
-		AOS.init({
-			// Global settings:
-			disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-			startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
-			initClassName: "aos-init", // class applied after initialization
-			animatedClassName: "aos-animate", // class applied on animation
-			useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-			disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-			debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-			throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-
-			// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-			offset: 120, // offset (in px) from the original trigger point
-			delay: 0, // values from 0 to 3000, with step 50ms
-			duration: 1000, // values from 0 to 3000, with step 50ms
-			easing: "ease", // default easing for AOS animations
-			once: true, // whether animation should happen only once - while scrolling down
-			mirror: false, // whether elements should animate out while scrolling past them
-			anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
-		});
-	}, []);
 
 	return (
 		<main className='overflow-x-hidden'>
